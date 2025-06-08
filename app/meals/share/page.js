1c54724f9d237999e26c15b35cdf8fd91a564e1f
devpool007 <sharmadevansh007777@gmail.com>
2025-06-09 00:20:29 +0200

    added some pages and did some routing and styling and made some client side components

---
+++ app/meals/share/page.js
@@ -0,0 +1,8 @@
+export default function MealsSharePage() {
+  return (
+    <div>
+      <h1>Share a Meal</h1>
+      <p>This is the share meal page.</p>
+    </div>
+  );
+}
---
+++ components/main-header/main-header.js
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import Image from "next/image";
+import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import MainHeaderBkg from "@/components/main-header/main-header-bkg";
+import NavLink from "./nav-link";
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBkg />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image
+            src={logoImg}
+            alt="A plate with food on it, representing a meal"
+            priority
+          />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+             <NavLink href="/meals">Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href="/community">Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
