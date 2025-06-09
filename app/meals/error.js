efe29e0eee6131044dba10693e2301ab039b1d3a
devpool007 <sharmadevansh007777@gmail.com>
2025-06-10 01:31:33 +0200

    added error page and not-found page next style

---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+'use client';
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>OOPS!</h1>
+      <p>We encountered an error while trying to load the meals.</p>
+      <p>Please try again later.</p>
+    </main>
+  );
+}
---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>404</h1>
+      <p>Sorry, we could not find the page you were looking for.</p>
+      <p>Please check the URL or return to the homepage.</p>
+    </main>
+  );
+}
