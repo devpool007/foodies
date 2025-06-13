1b8e54a226b2c520a2cd5cf22f1190c24229fbcd
devpool007 <sharmadevansh007777@gmail.com>
2025-06-13 17:23:02 +0200

    added vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,19 @@
-import './globals.css';
-import MainHeader from '@/components/main-header/main-header';
+import "./globals.css";
+import MainHeader from "@/components/main-header/main-header";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata = {
-  title: 'NextLevel Food',
-  description: 'Delicious meals, shared by a food-loving community.',
+  title: "NextLevel Food",
+  description: "Delicious meals, shared by a food-loving community.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        
         <MainHeader />
         {children}
+        <Analytics />
       </body>
     </html>
   );
